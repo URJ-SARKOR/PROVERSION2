70802364f3f601369672ed8e1214d69a797f3508
Your Name <you@example.com>
2023-08-23 15:08:56 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def dobavit(a: int, b: int) -> dict:
+    return {'keyword': a + b}
+print(dobavit( 66, 44 ))
+
+
+def umnojit(a: int, b: int) -> dict:
+    return {'keyword': a * b}
+print(umnojit( 60, 4 ))
+
+
+
